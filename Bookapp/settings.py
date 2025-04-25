c92ce079988e2dd7f53a67dbfda76f342a59d2f7
punitsinha23 <punitsinha495@gmail.com>
2025-04-25 13:48:26 +0530

    added data base

--- Bookapp/settings.py
+++ Bookapp/settings.py
@@ -76,13 +76,18 @@ WSGI_APPLICATION = 'Bookapp.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',  # Database name
+        'USER': 'postgres',  # Username
+        'PASSWORD': 'ndgTlwwlVUjyjEMDpEOVBUrUxsELKoET',  # Password
+        'HOST': 'mainline.proxy.rlwy.net',  # Host
+        'PORT': '19788',  # Port
     }
 }
+
+
 
 
 # Password validation
