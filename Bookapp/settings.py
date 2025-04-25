c8ee6d99686254281cc9573a00d32939295691aa
punitsinha23 <punitsinha495@gmail.com>
2025-04-25 12:48:11 +0530

    minor changes

--- Bookapp/settings.py
+++ Bookapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-g36e=)o_v%=)=^d(_wqbxq+_x-t1^1^9kyjqdx0#@ec+j(o30w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
